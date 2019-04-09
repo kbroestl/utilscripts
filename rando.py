e220bb6ccc55c74f3d8dfe77a8186caa50c44d33
Kent Broestl <kbroestl@innova-partners.com>
2019-04-09 14:51:01 -0400

    rando updates. Probably want to add some of my latex nonsense in here too.

--- rando.py
+++ rando.py
@@ -12,6 +12,7 @@ import string
 import random
 import argparse
 import itertools
+import re
 
 if __name__ == '__main__':
     
@@ -26,12 +27,24 @@ if __name__ == '__main__':
     if args.iterations:
         i = args.iterations
     else:
-        i = 5
+        i = 10
     
     if args.length:
         l = args.length
     else:
-        l = 6
+        l = 8
     
-    for _ in itertools.repeat(None, i):
-        print ''.join(random.choice(string.ascii_letters + string.digits) for x in range(l))
+    # for _ in itertools.repeat(None, i):
+    #     print ''.join(random.choice(string.ascii_letters + string.digits) for x in range(l))
+# passwords must have at least one uppercase
+# and at least one digit
+
+cap_re = re.compile('[A-Z]*')
+num_re = re.compile('\d')
+
+c = 1
+while c < i:
+    pwd = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(l))
+    if (re.search(cap_re, pwd) and re.search(num_re, pwd)):
+        print pwd
+        c += 1
